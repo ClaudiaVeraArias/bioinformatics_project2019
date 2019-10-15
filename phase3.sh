30d571cdb0048fa50b3f084ad3bc6316781a5ed4
Tom Moran <thomasemoran91@gmail.com>
2019-10-15 14:47:30 -0400

    Edit phase 3 so that the relative path is specified mase phase 4 to read hmmer

--- phase3.sh
+++ phase3.sh
@@ -1,7 +1,7 @@
 # This script will take our fasta files and run them through muscle
 
-muscle -in hsp70ref.fasta -out hsp70ref.afa
+~/Private/programs/muscle -in hsp70ref.fasta -out hsp70ref.afa
 
-muscle -in mcrAref.fasta -out mcrAref.afa
+~/Private/programs/muscle -in mcrAref.fasta -out mcrAref.afa
 
 
---
+++ phase4.sh
@@ -0,0 +1,9 @@
+# This script will take our files and run through hmmr
+
+~/Private/programs/hmmer-3.2/bin/hmmbuild hsp70ref.hmm hsp70ref.afa
+
+~/Private/programs/hmmer-3.2/bin/hmmsearch hsp70ref.hmm hsp70ref.fa
+
+
+
+
