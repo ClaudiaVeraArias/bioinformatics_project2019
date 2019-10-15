50da66da167378a3da50a27ab97119d9020a6078
Tom Moran <thomasemoran91@gmail.com>
2019-10-15 19:42:13 -0400

    modifications to VeraMoranscript.sh

--- VeraMoranscript.sh
+++ VeraMoranscript.sh
@@ -1,17 +1,26 @@
+# Usage: bash VeraMoranscript.sh <outputname>
+
 # This is a script to take all ref files and add to one file
 
-for file in hsp70gene_[0-9]{2}.fasta
+for file in *.fasta
 do
-    cat $file >> hsp70ref.fasta
+    cat $file >> $1.fasta
 done
 
 # This script will take our fasta files and run them through muscle
 
-~/Private/programs/muscle -in hsp70ref.fasta -out hsp70ref.afa
+~/Private/programs/muscle -in $1.fasta -out $1.afa
+rm $1.fasta
 
 # This script will take our files and run through hmmr
 
-~/Private/programs/hmmer-3.2/bin/hmmbuild hsp70ref.hmm hsp70ref.afa
+~/Private/programs/hmmer-3.2/bin/hmmbuild $1.hmm $1.afa
 
-~/Private/programs/hmmer-3.2/bin/hmmsearch hsp70ref.hmm hsp70ref.fa
+#for proteome in *.fasta
+#do
+#    ~/Private/programs/hmmer-3.2/bin/hmmsearch -o output.txt $1.hmm $proteome
+#    cat output.txt | grep 'target sequence' |
+#done
+
+
 
