b7c4b20b73b061b33004163b0b45d75b300a4088
Tom Moran <thomasemoran91@gmail.com>
2019-10-16 10:04:16 -0400

    Expanded flexibility to script

--- VeraMoranscript.sh
+++ VeraMoranscript.sh
@@ -1,20 +1,20 @@
-# Usage: bash VeraMoranscript.sh <outputname>
+# Usage: bash VeraMoranscript.sh <refdirectory> no slash
 
 # This is a script to take all ref files and add to one file
 
-for file in *.fasta
+for file in $@/*.fasta
 do
-    cat $file >> $1.fasta
+    cat $file >> $@.fasta
 done
 
 # This script will take our fasta files and run them through muscle
 
-~/Private/programs/muscle -in $1.fasta -out $1.afa
-rm $1.fasta
+~/Private/programs/muscle -in $@.fasta -out $@.afa
+rm $@.fasta
 
 # This script will take our files and run through hmmr
 
-~/Private/programs/hmmer-3.2/bin/hmmbuild $1.hmm $1.afa
+~/Private/programs/hmmer-3.2/bin/hmmbuild $@.hmm $@.afa
 
 #for proteome in *.fasta
 #do
