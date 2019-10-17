125098edc20e255ba17a5a079da59d92142c40a9
ClaudiaVeraArias <55985345+ClaudiaVeraArias@users.noreply.github.com>
2019-10-17 09:19:47 -0400

    samples table

--- Search_mcrA.sh
+++ Search_mcrA.sh
@@ -3,4 +3,5 @@
 for file in proteome*.fasta
 do
 /afs/crc.nd.edu/user/c/cvera/Private/programs/hmmer-3.2.1/bin/hmmsearch mcrArefHMM $file >> Search_mcrA.txt
+cat Search_hsp90.txt | egrep "^ {1,4}[0-9][.][0-9]*-?[0-9a-zA-Z]" Search_mcrA.txt > mcrAColumns.txt
 done
