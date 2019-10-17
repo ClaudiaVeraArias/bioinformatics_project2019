6e9b55380b830f9f0c26689c7fb2920b5cf5074c
ClaudiaVeraArias <55985345+ClaudiaVeraArias@users.noreply.github.com>
2019-10-17 09:13:17 -0400

    create a table with the samples

--- Search_hsp90.sh
+++ Search_hsp90.sh
@@ -3,4 +3,5 @@
 for file in proteome*.fasta
 do
 /afs/crc.nd.edu/user/c/cvera/Private/programs/hmmer-3.2.1/bin/hmmsearch hsp90HMM $file >> Search_hsp90.txt
+cat Search_hsp90.txt | egrep "^ {1,4}[0-9][.][0-9]*-?[0-9a-zA-Z]" Search_hsp90.txt > hsp90Columns.txt
 done
