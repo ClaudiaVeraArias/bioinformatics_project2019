1fa9d3afcd9baed013e207fec378803dc27d67e6
Tom Moran <thomasemoran91@gmail.com>
2019-10-18 10:35:18 -0400

    This is pretty much complete. Need to add psuedocode and usage details

--- Moran.sh
+++ Moran.sh
@@ -22,22 +22,39 @@ cat $file/* > refs.fasta
 
 # This part needs to run each proteome through
 # the hmmsearch and then pull out each alignment
-
+rm summaryfile_$file.txt
+echo $file >> summaryfile_$file.txt
 for filename in proteomes/*.fasta
 do
     ~/Private/programs/hmmer-3.2/bin/hmmsearch --tblout output_tblout.txt -o output.txt -A outputA.txt $file.hmm $filename
-    proteomename=$(cat output_tblout.txt | grep Target | pcregrep -o 'proteomes/\K[^ ]+$')
-    echo $proteomename >> summaryfile_$file.txt
+   # proteomename=$(cat output_tblout.txt | grep Target | pcregrep -o 'proteomes/\K[^ ]+$')
+   # echo $proteomename >> summaryfile_$file.txt
     numberofmatchingproteins=$(cat outputA.txt | pcregrep -o '^WP[^ ]+' | sort | uniq | wc -l)
-    echo "Number of matching proteins: $numberofmatchingproteins" >> summaryfile_$file.txt
-    lengthofmatch=$(cat outputA.txt | pcregrep -o '^WP[^ ]+' | sort | uniq | cut -d / -f 2)
-    proteinID=$(cat outputA.txt | pcregrep -o '^WP[^ ]+' | sort | uniq | cut -d / -f 1)
-    #descriptionofprotein=$(cat output.txt | pcregrep '>> WP' | sed -E 's/>> WP_[0-9]+[.]1  //')
-    echo "Protein ID    Match Location" >> summaryfile_$file.txt
-    paste <(printf %s "$proteinID") <(printf %s "$lengthofmatch") >> summaryfile_$file.txt
+   # echo "Number of matching proteins: $numberofmatchingproteins" >> summaryfile_$file.txt
+   #lengthofmatch=$(cat outputA.txt | pcregrep -o '^WP[^ ]+' | sort | uniq | cut -d / -f 2)
+   #proteinID=$(cat outputA.txt | pcregrep -o '^WP[^ ]+' | sort | uniq | cut -d / -f 1)
+   #descriptionofprotein=$(cat output.txt | pcregrep '>> WP' | sed -E 's/>> WP_[0-9]+[.]1  //')
+    #echo "Protein ID    Match Location" >> summaryfile_$file.txt
+    #paste <(printf %s "$proteomename") <(printf %s "$file") <(printf %s "$numberofmatchingproteins") >> summaryfile_$file.txt
+
+    echo "$numberofmatchingproteins" >> summaryfile_$file.txt
 
 done
 
 rm output_tblout.txt output.txt outputA.txt
 
 done
+
+echo This file has the info > Report.txt
+echo Proteome ID > proteomename.txt
+ls proteomes/ >> proteomename.txt
+paste proteomename.txt summaryfile_*.txt > Report.txt
+
+
+#for var in summaryfile*
+#do
+#    hit=$(cat $var | cut -f 3)
+#   paste <(printF %s "$hit")
+#done
+
+
